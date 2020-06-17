574235c783d2aa953051db46b19120140673172a
Sandro Santos <sandro.dev@gmail.com>
2020-06-17 17:13:37 -0300

    Create User Service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+import usersRoutes from '../routes/users.routes';
+
+interface Request {
+  name: string;
+
+  email: string;
+
+  password: string;
+}
+
+export default class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw Error('The user already exists');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
