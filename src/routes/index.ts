f6264046af422d2ccfbe28b1d6c7878c2042e942
Sandro Santos <sandro.dev@gmail.com>
2020-06-17 17:14:41 -0300

    users routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import appointmentsRoutes from './appointments.routes';
+import usersRoutes from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRoutes);
+routes.use('/users', usersRoutes);
 
 routes.get('/', (request, response) =>
   response.json({ message: 'Hello node and typescript' }),
