47b485d2b40981fbbb9fd7b6412b6acc0c1a81ef
Sandro Santos <sandro.dev@gmail.com>
2020-07-06 18:41:46 -0300

    multer upload config

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,13 @@ import multer from 'multer';
 import { resolve } from 'path';
 import crypto from 'crypto';
 
+const tempFolder = resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+  directory: tempFolder,
+
   storage: multer.diskStorage({
-    destination: resolve(__dirname, '..', '..', 'tmp'),
+    destination: tempFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
