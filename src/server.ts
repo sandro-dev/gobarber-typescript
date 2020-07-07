c18b86928a89b4c706667836e66d10fa0b5dd3b2
Sandro Santos <sandro.dev@gmail.com>
2020-07-07 13:08:35 -0300

    static images view on routes "/files/image"

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import routes from './routes';
 
 import 'reflect-metadata';
 import './database';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   console.log('✅ The server is running on port 3333');
